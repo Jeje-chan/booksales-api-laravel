f47b80c548e6cbbb49f659bf7b1140059978a7d3
Sharma Azzahrah <sharmaazzahrah02@gmail.com>
2025-05-19 17:02:23 +0700

    tugas 4

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,6 +10,10 @@ class AuthorController extends Controller
     public function index(){
         $authors = Author::all();
 
-        return view('authors', ['authors' => $authors]);
+        return response()->json([
+            "success" => true,
+            "message"=> "get all resources",
+            "data"=> $authors
+        ],200);
     }
 }
